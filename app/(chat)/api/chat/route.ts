11ca39f5da3d32618c7189d73c5dbd0f407a0b41
VMinB12 <vincent.min@b12-consulting.com>
2024-11-13 16:45:54 +0100

    add more custom data stream parsing logic

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -1,4 +1,4 @@
-import { LangChainAdapter, Message } from 'ai';
+import { Message } from 'ai';
 import { auth } from '@/app/(auth)/auth';
 import { deleteChatById, getChatById } from '@/db/queries';
 import { Client } from '@langchain/langgraph-sdk';
@@ -18,12 +18,12 @@ export async function POST(request: Request) {
   const assistantID = 'agent';
   const thread = await client.threads.create();
 
+  const lastHumanMessage = messages.find((message) => message.role === 'user');
+  if (!lastHumanMessage) {
+    return new Response('Bad Request', { status: 400 });
+  }
   const input = {
-    messages: messages.map((message) =>
-      message.role == 'user'
-        ? { role: 'user', content: message.content }
-        : { role: 'assistant', content: message.content }
-    ),
+    messages: [lastHumanMessage],
   };
 
   const config = { configurable: {} };
@@ -34,20 +34,69 @@ export async function POST(request: Request) {
     streamMode: 'events',
   });
 
-  const readableStream = new ReadableStream({
+  const encoder = new TextEncoder();
+
+  // ai-sdk has various internal codes for dataStream responses.
+  // see this link for information about the codes:
+  // https://github.com/vercel/ai/blob/main/packages/ui-utils/src/stream-parts.ts
+  const ai_code = 0;
+
+  const stream = new ReadableStream({
     async start(controller) {
       for await (const chunk of streamResponse) {
-        if (chunk.event.startsWith('events')) {
-          controller.enqueue(chunk.data);
-        }
         console.log(`Receiving new event of type: ${chunk.event}...`);
         console.log('CHUNK.DATA.EVENT:', chunk.data.event);
+        if (
+          chunk.data.event &&
+          chunk.data.event.startsWith('on_chat_model') &&
+          chunk.data.data
+        ) {
+          console.log('CHUNK.DATA.DATA:', chunk.data.data);
+          // controller.enqueue(chunk.data.data);
+        }
+        if (
+          chunk.data.event &&
+          chunk.data.event === 'on_chat_model_end' &&
+          chunk.data.data
+        ) {
+          // Example of chunk.data.data:
+          // {
+          //   output: {
+          //     content: 'There are 10 movies in the database.',
+          //     additional_kwargs: {},
+          //     response_metadata: {},
+          //     type: 'ai',
+          //     name: null,
+          //     id: 'run-9830865a-6379-45a8-a501-cbf8d3887790-0',
+          //     example: false,
+          //     tool_calls: [],
+          //     invalid_tool_calls: [],
+          //     usage_metadata: null
+          //   },
+          //   input: { messages: [ [Array] ] }
+          // }
+          const content = chunk.data.data.output.content;
+          const formattedContent = `${ai_code}:${JSON.stringify(content)}\n`;
+          controller.enqueue(encoder.encode(formattedContent));
+        }
+        // if (chunk.event === 'events') {
+        //   controller.enqueue(chunk.data);
+        // }
+        // if (chunk.data.event === 'on_chat_model_stream') {
+        //   controller.enqueue(chunk.data?.data);
+        // }
       }
       controller.close();
     },
   });
 
-  return LangChainAdapter.toDataStreamResponse(readableStream);
+  return new Response(stream, {
+    status: 200,
+    headers: new Headers({
+      'Content-Type': 'text/plain; charset=utf-8',
+      'X-Vercel-AI-Data-Stream': 'v1',
+    }),
+  });
 }
 
 export async function DELETE(request: Request) {
