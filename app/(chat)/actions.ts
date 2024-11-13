f0d3778584224480f221fd5c6e56644ccb1396bb
VMinB12 <vincent.min@b12-consulting.com>
2024-11-13 21:53:56 +0100

    use cheaper gpt-4o for summarizing conversations

--- app/(chat)/actions.ts
+++ app/(chat)/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { CoreMessage, CoreUserMessage, generateText } from 'ai';
+import { CoreUserMessage, generateText } from 'ai';
 import { cookies } from 'next/headers';
 
 import { customModel } from '@/ai';
@@ -16,9 +16,8 @@ export async function generateTitleFromUserMessage({
   message: CoreUserMessage;
 }) {
   const { text: title } = await generateText({
-    model: customModel('gpt-3.5-turbo'),
-    system: `\n
-    - you will generate a short title based on the first message a user begins a conversation with
+    model: customModel('gpt-4o-mini'),
+    system: `- you will generate a short title based on the first message a user begins a conversation with
     - ensure it is not more than 80 characters long
     - the title should be a summary of the user's message
     - do not use quotes or colons`,
